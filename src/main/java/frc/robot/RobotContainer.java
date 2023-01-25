fc9c69e5bd9d5621d636ae14f7831ff0805635c0
PreedomCL <54563759+PreedomCL@users.noreply.github.com>
2023-01-24 20:55:37 -0700

    Fix import errors in RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,8 +4,6 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.subsystems.Playground;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
