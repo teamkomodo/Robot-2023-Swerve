e9b6a655eb73b48f658914f5d6fc25bba7bfa74e
Ray <ranchordo@gmail.com>
2023-02-27 19:54:42 -0700

    Revert "Merge pull request #9 from teamkomodo/scrimmage-merge"
    This reverts commit 2b55e44ad86ba8b3f7dea2d8000404085447a5ef, reversing
    changes made to af233011654e455f58b183ecc572f72d3edbd052.
    
    maybe?

--- README.md
+++ README.md
@@ -7,7 +7,4 @@
 ## Autonomous
 
 ## Electrical
-
-## [Link to Pinouts and Robot Parameters](https://docs.google.com/spreadsheets/d/10XPUEvFrafupFar4bhCG224BRXnX_bT2MSt10pouS7c/edit?usp=sharing)
-
-![Electrical Diagram](https://raw.githubusercontent.com/teamkomodo/Robot-2023-Swerve/main/Documentation/Electrical/ElectricalDiagram.jpg)
+![Electrical Diagram](https://raw.githubusercontent.com/teamkomodo/Robot-2023-Swerve/main/Documentation/Electrical/Electrical%20System%20Diagram.drawio.png)
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,12 +4,11 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.SwerveControllerCommandFactory;
-import frc.robot.commands.auto.AutoDefinitions;
 import frc.robot.commands.auto.AutoLevelCommand;
-import frc.robot.subsystems.ClawSubsystem;
+import frc.robot.commands.SwerveControllerCommandFactory;
+import frc.robot.commands.auto.AlignToGamePiece;
+import frc.robot.commands.auto.AutoDefinitions;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.TrajectorySequencer;
 import frc.robot.subsystems.VisionPositioningSubsystem;
@@ -20,26 +19,14 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.subsystems.ElevatorSubsystem;
-import frc.robot.subsystems.JointSubsystem;
-import frc.robot.subsystems.LEDStripSubsystem;
-import frc.robot.subsystems.TelescopeSubsystem;
 
 import static frc.robot.Constants.*;
-
-import static frc.robot.util.Util.*;
 
 public class RobotContainer {
     private final Field2d field2d = new Field2d();
 
     // Subsystem definitions should be public for auto reasons
-    public final ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem();
-    public final TelescopeSubsystem telescopeSubsystem = new TelescopeSubsystem();
-    public final JointSubsystem jointSubsystem = new JointSubsystem();
-    public final ClawSubsystem clawSubsystem = new ClawSubsystem();
-    //public final LEDStripSubsystem ledStripSubsystem = new LEDStripSubsystem();
     public final DrivetrainSubsystem drivetrainSubsystem = new DrivetrainSubsystem(field2d);
     public final SwerveControllerCommandFactory sccf = new SwerveControllerCommandFactory(drivetrainSubsystem);
     public final TrajectorySequencer trajectorySequencer = new TrajectorySequencer(drivetrainSubsystem, sccf, null,
@@ -62,85 +49,35 @@ public class RobotContainer {
     }
 
     private void configureBindings() {
+        Trigger slowModeButton = driverXBoxController.leftBumper();
+        Trigger autoLevelButton = driverXBoxController.a();
+        autoLevelButton.whileTrue(new AutoLevelCommand(drivetrainSubsystem));
 
-        Trigger aButton = driverXBoxController.a();
-        Trigger bButton = driverXBoxController.b();
-        Trigger xButton = driverXBoxController.x();
-        Trigger yButton = driverXBoxController.y();
-        Trigger leftBumper = driverXBoxController.leftBumper();
-        Trigger rightBumper = driverXBoxController.rightBumper();
-
-        Trigger leftJoystickYPositive = driverXBoxController.axisGreaterThan(XboxController.Axis.kLeftY.value,
-                XBOX_JOYSTICK_THRESHOLD);
-        Trigger leftJoystickYNegative = driverXBoxController.axisLessThan(XboxController.Axis.kLeftY.value,
-                -XBOX_JOYSTICK_THRESHOLD);
-        Trigger leftJoystickY = leftJoystickYPositive.or(leftJoystickYNegative);
-
-        Trigger leftJoystick = driverXBoxController.leftStick();
-        Trigger rightJoystick = driverXBoxController.rightStick();
-
-        Trigger leftTrigger = driverXBoxController.leftTrigger();
-        Trigger rightTrigger = driverXBoxController.rightTrigger();
-
-        Trigger rightDriverJoystickButton = new JoystickButton(driverJoystick, 1);
-        Trigger leftDriverJoystickButton = new JoystickButton(driverJoystick, 2);
-
-        //Elevator Triggers
-        leftJoystick.whileTrue(Commands.run(
-                () -> elevatorSubsystem.setMotorPercent(driverXBoxController.getLeftY()),
-                elevatorSubsystem)).onFalse(Commands.run(() -> elevatorSubsystem.setMotorPercent(0), elevatorSubsystem));
-
-        //Drivetrain triggers
         drivetrainSubsystem.setDefaultCommand(
                 Commands.run(
                         () -> drivetrainSubsystem.drive(
-                                joystickCurve(-driverJoystick.getRawAxis(1))
-                                        * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
-                                joystickCurve(-driverJoystick.getRawAxis(0))
-                                        * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
-                                joystickCurve(driverJoystick.getRawAxis(2))
-                                        * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+                                -driverXBoxController.getLeftY() * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+                                -driverXBoxController.getLeftX() * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+                                -driverXBoxController.getRightX() * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
                                 true),
                         drivetrainSubsystem));
 
-        leftBumper.whileTrue(
-            Commands.run(
-                    () -> drivetrainSubsystem.drive(
-                            joystickCurve(driverXBoxController.getLeftY())
-                                    * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND * SLOW_MODE_MODIFIER,
-                            joystickCurve(driverXBoxController.getLeftX())
-                                    * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND * SLOW_MODE_MODIFIER,
-                            joystickCurve(driverXBoxController.getRightX())
-                                    * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND * SLOW_MODE_MODIFIER,
-                            false),
-                    drivetrainSubsystem));
-
-        rightDriverJoystickButton.whileTrue(new AutoLevelCommand(drivetrainSubsystem));
-        leftDriverJoystickButton.onTrue(Commands.runOnce(() -> drivetrainSubsystem.zeroGyro()));
-
-        //Claw Triggers
-        rightBumper.whileTrue(clawSubsystem.openCommand());
-        leftBumper.whileTrue(clawSubsystem.closeCommand());
-
-        //Joint Triggers
-        rightTrigger.whileTrue(Commands.run(
-            () -> jointSubsystem.setMotorPercent(-driverXBoxController.getRightTriggerAxis()),
-            jointSubsystem).andThen(() -> jointSubsystem.setMotorPercent(0), jointSubsystem));
-        leftTrigger.whileTrue(Commands.run(
-            () -> jointSubsystem.setMotorPercent(driverXBoxController.getLeftTriggerAxis()),
-            jointSubsystem).andThen(() -> jointSubsystem.setMotorPercent(0), jointSubsystem));
-    
-        
-        //Telescope Triggers
-        rightJoystick.whileTrue(Commands.run(
-            () -> telescopeSubsystem.setMotorPercent(driverXBoxController.getRightY()),
-            telescopeSubsystem).andThen(() -> telescopeSubsystem.setMotorPercent(0), telescopeSubsystem));
+        slowModeButton.whileTrue(
+                Commands.run(
+                        () -> drivetrainSubsystem.drive(
+                                -driverXBoxController.getLeftY() * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND
+                                        * SLOW_MODE_MODIFIER,
+                                -driverXBoxController.getLeftX() * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND
+                                        * SLOW_MODE_MODIFIER,
+                                -driverXBoxController.getRightX() * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND
+                                        * SLOW_MODE_MODIFIER,
+                                true),
+                        drivetrainSubsystem));
     }
 
     public Command getAutonomousCommand() {
-        return null;
-        // autonomousController.initAutonomous();
-        // return autonomousController.chooser.getSelected().generateCommand();
+        autonomousController.initAutonomous();
+        return autonomousController.chooser.getSelected().generateCommand();
         // return new AlignToGamePiece(drivetrainSubsystem, detector, 0);
     }
 }
